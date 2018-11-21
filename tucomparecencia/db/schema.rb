4bc732efd8109be2c240f5c7eb09817a7dfbceea
Ricardo Suarez <rsuarez.adv@gmail.com>
2018-11-20 21:43:20 -0300

    ADD: Se agrega scaffold de documentos

--- tucomparecencia/db/schema.rb
+++ tucomparecencia/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_001423) do
+ActiveRecord::Schema.define(version: 2018_11_21_004154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2018_11_21_001423) do
 
   create_table "communes", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "documents", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
