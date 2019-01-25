13ea25bad0ed94c5465a00bfa93de20dcc5e3483
Ricardo Suarez <rsuarez.adv@gmail.com>
2019-01-25 00:25:20 -0300

    ADD: .evn file

--- tucomparecencia/app/controllers/documents_controller.rb
+++ tucomparecencia/app/controllers/documents_controller.rb
@@ -12,12 +12,12 @@ class DocumentsController < ApplicationController
   # GET /documents/1.json
   def show
     respond_to do |format|
- format.html
- format.pdf do
-   render template: 'documents/show', pdf: 'Document Detail Report-' + Time.now.strftime('%v %H:%M:%S').to_s, javascript_delay: 10000,
-   layout: 'pdf_layout.html.haml', disposition: 'attachment'
- end
-end
+      format.html
+      format.pdf do
+        render template: 'documents/show', pdf: 'Document Detail Report-' + Time.now.strftime('%v %H:%M:%S').to_s, javascript_delay: 10000,
+        layout: 'pdf_layout.html.haml', disposition: 'attachment'
+      end
+    end
   end
 
   # GET /documents/new
